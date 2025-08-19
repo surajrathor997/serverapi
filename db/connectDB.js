2c915ef6773bd34d5316b5afe5cfb86dcae726d5
surajrathor997 <surajrathor8989@gmail.com>
2025-08-19 11:17:50 +0530

    fix: update environment variable name for database connection

--- db/connectDB.js
+++ db/connectDB.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose')
 
-const Live_url = "mongodb+srv://bookreview123:Suraj123@cluster1.ilco5g2.mongodb.net/bookreview?retryWrites=true&w=majority&appName=Cluster1"
+// const Live_url = "mongodb+srv://bookreview123:Suraj123@cluster1.ilco5g2.mongodb.net/bookreview?retryWrites=true&w=majority&appName=Cluster1"
 
 const connectDB = async ()=>{
-    return mongoose.connect(process.env.Live_url)
+    return mongoose.connect(process.env.LIVE_URL)
 
     .then(()=>{
         console.log("Database connection successfully")
